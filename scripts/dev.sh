10f1b58ffbcf1ed691a0327117e20a4bc1a6010c
memothelemo <dev@memothelemo.xyz>
2024-10-18 20:43:30 +0800

    Arrange the codebase a bit

--- scripts/dev.sh
+++ scripts/dev.sh
@@ -10,4 +10,4 @@ rojo serve test-build.project.json \
     & rojo sourcemap default.project.json -o sourcemap.json --watch \
     & rojo sourcemap test.project.json -o sourcemap.test.json --watch \
     & darklua process --config .darklua.json --watch src/ out/library \
-    & darklua process --config .darklua.test.json --watch test/ out/test
+    & darklua process --config .darklua.test.json --watch tests/ out/tests

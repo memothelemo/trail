e15ff80e19f3f5f13bdb91a206044503be3039e5
memothelemo <dev@memothelemo.xyz>
2024-10-18 21:17:13 +0800

    Add trail-subscriber package

--- scripts/dev.sh
+++ scripts/dev.sh
@@ -9,5 +9,7 @@ fi
 rojo serve build.project.json \
     & rojo sourcemap default.project.json -o sourcemap.json --watch \
     & rojo sourcemap modules/trail/default.project.json -o modules/trail/sourcemap.json --watch \
+    & rojo sourcemap modules/trail-subscriber/default.project.json -o modules/trail-subscriber/sourcemap.json --watch \
     & darklua process --config modules/trail/.darklua.json --watch modules/trail/src modules/trail/out \
+    & darklua process --config modules/trail-subscriber/.darklua.json --watch modules/trail-subscriber/src modules/trail-subscriber/out \
     & darklua process --config .darklua.json --watch tests/ out/
